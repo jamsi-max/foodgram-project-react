ba96c08c00e2e9ba110d8d58d2179cf07e3ac0de
Maxim Nemkov <dedau@bk.ru>
2022-07-17 02:16:49 +0300

    add PDF download

--- backend/backend/users/admin.py
+++ backend/backend/users/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
+
 from .models import FoodUser
 
 
 class FoodUserAdmin(admin.ModelAdmin):
-    list_display = ('email','username', 'folower_count', 'folowing_count')
+    list_display = ('email','username', 'folower_count', 'folowing_count',)
     list_filter = ('username', )
     search_fields = ('username',)
     ordering = ('username',)
+
 
 admin.site.register(FoodUser, FoodUserAdmin)
