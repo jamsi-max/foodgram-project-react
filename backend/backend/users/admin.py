502430238a777bca8e8f02a8d884c0fcc868cc65
Maxim Nemkov <dedau@bk.ru>
2022-07-17 02:19:45 +0300

    add PDF download

--- backend/backend/users/admin.py
+++ backend/backend/users/admin.py
@@ -4,8 +4,11 @@ from .models import FoodUser
 
 
 class FoodUserAdmin(admin.ModelAdmin):
-    list_display = ('email','username', 'folower_count', 'folowing_count',)
-    list_filter = ('username', )
+    list_display = ('email',
+                    'username',
+                    'folower_count',
+                    'folowing_count',)
+    list_filter = ('username',)
     search_fields = ('username',)
     ordering = ('username',)
 
