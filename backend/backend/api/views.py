cd83d58d8178aa54b2f2412d4530e469f44c2900
Maxim Nemkov <dedau@bk.ru>
2022-07-18 01:14:24 +0300

    fixed recipe permission

--- backend/backend/api/views.py
+++ backend/backend/api/views.py
@@ -15,7 +15,7 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.pagination import PageNumberPagination
+# from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import (HTTP_200_OK, HTTP_201_CREATED,
