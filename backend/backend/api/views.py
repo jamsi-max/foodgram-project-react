64d66de5fdf2277445a0efe73f587d01dc4fc02b
Maxim Nemkov <dedau@bk.ru>
2022-07-17 12:30:40 +0300

    add PDF download

--- backend/backend/api/views.py
+++ backend/backend/api/views.py
@@ -1,8 +1,9 @@
 
 import io
+
+from django.contrib.auth import get_user_model
 from django.db.models import Sum
 from django.http import FileResponse
-from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from core.models import BasketUser
 from django_filters.rest_framework import DjangoFilterBackend
--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -9,6 +9,7 @@ whoami
 
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
+python manage.py makemigrations
 python manage.py migrate
 python manage.py addcsvdata
 
